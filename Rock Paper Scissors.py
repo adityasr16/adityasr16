8681aca18d67068560db4ae89dee90fabaf34775
adityasr16 <88029844+adityasr16@users.noreply.github.com>
2021-09-03 14:23:17 +0530

    Add files via upload

---
+++ Rock Paper Scissors.py
@@ -0,0 +1,38 @@
+import sys
+
+player1 = input("What is your name player 1? ")
+player2 = input("What is your name player 2? ")
+
+player1_move = input("player 1's move: ").lower()
+player2_move = input("player 2's move: ").lower()
+
+def game(move_1,move_2):
+    
+    if move_1 == move_2:
+        return ("It's a tie")
+    elif move_1 == "rock":
+        if move_2 == "scissor":
+            return ("Player 1 wins!")
+        elif move_2 == "paper":
+            return ("player 2 wins!")
+    elif move_1 == "scissor":
+        if move_2 == "paper":
+            return ("player 1 wins!")
+        elif move_2 == "rock":
+            return ("player 2 wins!")
+        
+    elif move_1 == "paper":
+        if move_2 == "rock":
+            return ("player 1 wins!")
+        elif move_2 == "scissor":
+            return ("player 2 wins!")
+    else:
+        return ("*warning* invalid input.It's a rock paper scissor game so enter either rock,paper or scissor ")
+        
+        sys.exit()
+        
+print(game(player1_move,player2_move))
+
+        
+    
+            
